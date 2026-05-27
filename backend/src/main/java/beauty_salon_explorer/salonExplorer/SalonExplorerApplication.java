f8a0bba6d68f808217368a75757a6985e35c93bc
Kacper <kacperx123@gmail.com>
2026-05-27 20:45:17 +0200

    initialize spring boot backend

---
+++ backend/src/main/java/beauty_salon_explorer/salonExplorer/SalonExplorerApplication.java
@@ -0,0 +1,13 @@
+package beauty_salon_explorer.salonExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonExplorerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/beauty_salon_explorer/salonExplorer/SalonExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package beauty_salon_explorer.salonExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
